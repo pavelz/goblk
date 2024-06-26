3a344aa3f0e849e5633bc6b1b85bd268be709733
Pavel Zaitsev <pavel@arslogi.ca>
2024-06-25 20:40:35 -0400

    add silly makefile

--- miner/miner.go
+++ miner/miner.go
@@ -4,3 +4,10 @@ type Miner interface {
 	Mine(mine_path string) (error)
 	LastTransaction(mine_path string) (int)
 }
+
+type Mine struct {
+}
+
+func (m Mine) Mine(mine_path string) (error) {
+	return nil
+}
