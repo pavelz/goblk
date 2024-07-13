fa11c14f80a2f0b12717ca6fdbeb357959ccd47e
Pavel Zaitsev <pavel@arslogi.ca>
2024-07-13 02:00:30 -0400

    get block

--- block_store/block_store.go
+++ block_store/block_store.go
@@ -2,9 +2,9 @@ package block_store
 
 import (
 	"encoding/binary"
+	"io"
 	"os"
 )
-
 const INDEX_SIZE uint = 0xffff
 
 type BlockStore struct {
@@ -16,10 +16,11 @@ func Open(path string) (*BlockStore, error) {
 	var store = BlockStore{index: make([]uint64, INDEX_SIZE, INDEX_SIZE)}
 
   file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0644) 
-	store.file = file // ngl this is shit
+
 	if err != nil {
 		panic("could not open storage file: " + err.Error())
 	} else {
+		store.file = file // ngl this is shit
 		binary.Read(file, binary.LittleEndian, store.index)
 	}
 
@@ -33,6 +34,11 @@ func Open(path string) (*BlockStore, error) {
 // index + 1  = blocks size
 // keep semantis in one place
 
-func (b * BlockStore) BlockAt(at uint64) ([]byte, uint){
+func (b * BlockStore) BlockAt(at uint64) ([]byte){
 	offset, size := b.index[at], b.index[at + 1]
+	block := make([]byte, size, size)
+
+	b.file.Seek(int64(offset),io.SeekStart)
+	b.file.Read(block)
+	return block
 }
