2bc31440dfc4aae5173cd92fd4e982b76e0e29fe
Pavel Zaitsev <pavel@arslogi.ca>
2024-09-01 17:25:53 -0400

    fix

--- hello.go
+++ hello.go
@@ -11,9 +11,11 @@ import (
 func main() {
 	hash := md5.Sum([]byte("hex me"))
 	text := hex.EncodeToString(hash[:])
+  
 	a := chain.NewChain()
   b := chain.Block{Previous: "two", From: "me", To: "who"}
   a.AddBlock(&b)
+
 	a.WriteChain("chain.json")
 
 	// load chain
@@ -22,6 +24,7 @@ func main() {
 	if err != nil {
 		println("there was an error: " + err.Error())
 	}
+
 	println(json.Marshal(*chain))
 	
 	fmt.Printf("Hello, World! %s %s\n", text, a.GetChecksum())
